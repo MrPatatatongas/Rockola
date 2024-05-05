c2287e3cb47ca709c9abdde1b5a0e43b908bc3cd
Mr.Patata <59590174+MrPatatatongas@users.noreply.github.com>
2024-05-05 14:30:32 -0600

    Terminando de programar
    No el trabajo mas limpio pero funciona

--- Tarea_Rockola.m
+++ Tarea_Rockola.m
@@ -17,7 +17,10 @@ warndlg('Placa acoplada!')
 Smoker=["Otro atardecer-Badbunny.mp3";"Memories-David Guetta.ogg";...
     "Shorty Party-Cartel de Santa.mp3";"Hundred_Miles.ogg";...
     "I_took_a_pill_in_Ibiza.wav";"Lou_Lou.wav";"Love_Goes.wav";...
-    "Una_vaina_loca.ogg";"Ven_Bailalo.mp4";"Zapata.mp4";"Animals.mp3"];
+    "Una_vaina_loca.ogg";"Ven_Bailalo.mp4";"Zapata.mp4";"Animals.mp3";...
+    "Mirrors - Caravan Palace.mp3";"Saeed - Infected Mushroom.mp3";...
+    "Californication - Red Hot Chili Peppers.mp3";...
+    "Alan Sutton - TUTANKMON.mp3"];
 Ar=Smoker
 %Seleccionamos la opcion
 k=input('Dame el numero de cancion que deseas reproducir:_');
@@ -46,21 +49,25 @@ end
 
 Velocidad=xv;
 Volumen=xr;
-
+%setteamos la velocidad, el volumen y el nombre de la cancion seleccionada
+%en equalizador
 h=equalizador(Velocidad,Volumen,Ar(k));
 alpha=h.setLoadPlay;
-%Insertamos las monedas para que inicie la rockola
+%Volvemos a inicializar Stop
 Stop=1;
-while Stop
+%Pedimos las 10 monedas al usuario
+warndlg('Inserte 10 monedas')
+%Insertamos las monedas para que inicie la rockola
+while Stop  %Usamos este while infinto para leer las pulsaciones
     p=digitalRead(a,2)  %leemos la pulsacion del boton
-    tc=tc+p     %Guardamos la cantidad de veces presionado
+    tc=tc+p     %Guardamos la cantidad de veces presionado en tc
     switch estado
-    %15 pinshis "cases" para cada opcion
+    %15 pinshis "cases" para cada cancion
         case 1
             if (k==1) && (tc==10)
                 warndlg('Haz escogido la cancion de Badbunny - Otro atardecer')
                 K=1;    %este apuntador tiene inicializado la cancion 1
-                while Stop
+                while Stop  %Usamos este while infinito para el menu
                     Menu_rockola
                 end
                 pause(0.25)
@@ -71,9 +78,10 @@ while Stop
         case 2
             if (k==2) && (tc==10)
                 warndlg('Haz escogido la cancion de David Guetta - Memories')
-                K=2;    %este apuntador tiene inicializado la cancion 1
-                play(alpha)
-                Stop=0;
+                K=2;    %este apuntador tiene inicializado la cancion 2
+                while Stop
+                    Menu_rockola
+                end
                 pause(0.25)
             else
                 estado=3;
@@ -82,13 +90,154 @@ while Stop
         case 3
             if (k==3) && (tc==10)
                 warndlg('Haz escogido la cancion de Cartel de Santa - Shorty Party')
-                K=3;    %este apuntador tiene inicializado la cancion 1
-                play(alpha)
-                Stop=0;
+                K=3;    %este apuntador tiene inicializado la cancion 3
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=4;
+            end
+        case 4
+            if (k==4) && (tc==10)
+                warndlg('Haz escogido la cancion Hundred Miles')
+                K=4;    %este apuntador tiene inicializado la cancion 4
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=5;
+            end
+        case 5
+            if (k==5) && (tc==10)
+                warndlg('Haz escogido la cancion I took a pill in Ibiza')
+                K=5;    %este apuntador tiene inicializado la cancion 5
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=6;
+            end
+        case 6
+            if (k==6) && (tc==10)
+                warndlg('Haz escogido la cancion Lou Lou')
+                K=6;    %este apuntador tiene inicializado la cancion 6
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=7;
+            end
+        case 7
+            if (k==7) && (tc==10)
+                warndlg('Haz escogido la cancion Love Goes')
+                K=7;    %este apuntador tiene inicializado la cancion 7
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=8;
+            end
+        case 8
+            if (k==8) && (tc==10)
+                warndlg('Haz escogido la cancion Una vaina loca')
+                K=8;    %este apuntador tiene inicializado la cancion 8
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=9;
+            end
+        case 9
+            if (k==9) && (tc==10)
+                warndlg('Haz escogido la cancion Ven bailalo')
+                K=9;    %este apuntador tiene inicializado la cancion 9
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=10;
+            end
+        case 10
+            if (k==10) && (tc==10)
+                warndlg('Haz escogido la cancion Zapata')
+                K=10;    %este apuntador tiene inicializado la cancion 10
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=11;
+            end
+        case 11
+            if (k==11) && (tc==10)
+                warndlg('Haz escogido la cancion Animals')
+                K=11;    %este apuntador tiene inicializado la cancion 11
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=12;
+            end
+        case 12
+            if (k==12) && (tc==10)
+                warndlg('Haz escogido la cancion Mirrors - Caravan Palace')
+                K=12;    %este apuntador tiene inicializado la cancion 11
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=13;
+            end
+        case 13
+            if (k==13) && (tc==10)
+                warndlg('Haz escogido la cancion Saeed - Infected Mushroom')
+                K=13;    %este apuntador tiene inicializado la cancion 11
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=14;
+            end
+        case 14
+            if (k==14) && (tc==10)
+                warndlg('Haz escogido la cancion Californication - RHCP')
+                K=14;    %este apuntador tiene inicializado la cancion 11
+                while Stop
+                    Menu_rockola
+                end
+                pause(0.25)
+            else
+                estado=15;
+            end
+        case 15
+            if (k==15) && (tc==10)
+                warndlg('Haz escogido la cancion Tutankmon - Alan Sutton')
+                K=15;    %este apuntador tiene inicializado la cancion 11
+                while Stop
+                    Menu_rockola
+                end
                 pause(0.25)
             else
                 estado=1;
-            end 
+            end
     end
     pause(0.25)
+end
+%Preguntamos por una segunda ronda del programa
+opc=questdlg('Desea correr nuevamente el programa',...
+    'Ventana emergente','Si','No','No');
+if strcmp(opc,'Si')
+    Tarea_Rockola
+else
+    warndlg('Proceso terminado')
 end
